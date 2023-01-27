a1b6c3674f41d2eae738a3e7b27fe2569adec5a1
Vishnudas2003 <vishnudas7412@gmail.com>
2023-01-27 10:26:35 -0500

    Updated every changes

---
+++ tests/unit/app.test.js
@@ -0,0 +1,9 @@
+// tests/unit/app.test.js
+
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('error 404', () => {
+    test('404 error', () => request(app).get('/wrongPath').expect(404));
+});
